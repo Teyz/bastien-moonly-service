7a85071052273316bf1ab61637bbf83176377f54
Bastien Rigaud <bastien.rigaud@spendesk.com>
2021-12-09 20:30:23 +0100

    feat(cryptosController): add cryptosController

---
+++ src/controller/cryptos/cryptos.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cryptos')
+export class CryptosController {
+    
+    @Get()
+    findAll(): string {
+        return 'This action returns all cryptos';
+    }
+}
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CryptosController } from 'src/controller/cryptos/cryptos.controller';
 import { AppController } from '../controller/app.controller';
 import { AppService } from '../services/app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CryptosController],
   providers: [AppService],
 })
 export class AppModule {}
